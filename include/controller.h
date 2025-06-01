07357bf38a704fbcb21f5324a48110dad173badf
andrinwinzap <AndrinWinzap@proton.me>
2025-06-01 17:23:09 +0200

    add base for controller

---
+++ include/controller.h
@@ -0,0 +1,4 @@
+// Serial Communication Protocol
+#define PROTOCOL_ADDRESS 0x01
+#define RXD2 26
+#define TXD2 27
--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -1,9 +1,82 @@
 #include <Arduino.h>
+#include <Serialization.h>
+#include <SerialProtocol.h>
+#include <Trajectory.h>
+#include <debug_macro.h>
+#include <controller.h>
+
+HardwareSerial com_serial(2);
+SerialProtocol com(com_serial, PROTOCOL_ADDRESS);
+
+void parse_cmd(uint8_t cmd, const uint8_t *payload, size_t payload_len)
+{
+    switch (cmd)
+    {
+    case Byte::Command::PING:
+    {
+        DBG_PRINTLN("[CMD] PING");
+        com.send_packet(Byte::Address::MASTER, Byte::Command::ACK);
+        break;
+    }
+    case Byte::Command::HOME:
+    {
+        DBG_PRINTLN("[CMD] HOME");
+        // NOT IMPLEMENTED
+        com.send_packet(Byte::Address::MASTER, Byte::Command::ACK);
+        break;
+    }
+    case Byte::Command::POS:
+    {
+        DBG_PRINTLN("[CMD] POS");
+        // NOT IMPLEMENTED
+        com.send_packet(Byte::Address::MASTER, Byte::Command::POS, payload, 4);
+        break;
+    }
+    case Byte::Command::LOAD_TRAJ:
+    {
+        DBG_PRINTLN("[CMD] LOAD_TRAJ");
+        // NOT IMPLEMENTED
+        com.send_packet(Byte::Address::MASTER, Byte::Command::ACK);
+        break;
+    }
+    case Byte::Command::EXEC_TRAJ:
+    {
+        DBG_PRINTLN("[CMD] EXEC_TRAJ");
+        // NOT IMPLEMENTED
+        com.send_packet(Byte::Address::MASTER, Byte::Command::ACK);
+        break;
+    }
+
+    default:
+        DBG_PRINT("[CMD] Unknown command: 0x");
+        DBG_PRINTLN(cmd, HEX);
+        DBG_PRINT("[CMD] Payload: ");
+        for (int i = 0; i < payload_len; i++)
+        {
+            DBG_PRINT("0x");
+            DBG_PRINT(payload[i], HEX);
+            DBG_PRINT(" ");
+        }
+        DBG_PRINTLN("");
+        break;
+    };
+}
 
 void setup()
 {
+    Serial.begin(115200);
+    com_serial.begin(115200, SERIAL_8N1, RXD2, TXD2);
+    DBG_PRINTLN("[SETUP] Setup complete");
 }
 
 void loop()
 {
+    if (com.available() > 0)
+    {
+        const Command *cmd = com.read();
+        if (cmd)
+        {
+            parse_cmd(cmd->cmd, cmd->payload, cmd->payload_len);
+        }
+    }
 }
