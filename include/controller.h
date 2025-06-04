dd4980caf29cde4b158c2cd2a7a9f45d4c06a168
andrinwinzap <AndrinWinzap@proton.me>
2025-06-04 09:09:21 +0200

    refactor

--- include/controller.h
+++ include/controller.h
@@ -1,7 +1,7 @@
 // Serial Communication Protocol
-#define PROTOCOL_ADDRESS 0x01
-#define RXD2 26
-#define TXD2 27
+constexpr int PROTOCOL_ADDRESS = 0x01;
+constexpr int RXD2 = 26;
+constexpr int TXD2 = 27;
 constexpr int MUX_S0 = 4;
 constexpr int MUX_S1 = 5;
 constexpr int MUX_S2 = 6;
