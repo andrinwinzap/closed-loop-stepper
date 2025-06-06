76a3a3ba7bc9bd7945abbf5a0d97a470e0ea47cc
andrinwinzap <AndrinWinzap@proton.me>
2025-06-06 18:11:07 +0200

    fix string conversion warning

--- include/credentials_example.h
+++ include/credentials_example.h
@@ -1,7 +1,7 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
-constexpr char *WIFI_SSID = "SSID";
-constexpr char *WIFI_PASS = "PASSWORD";
+constexpr const char *WIFI_SSID = "SSID";
+constexpr const char *WIFI_PASS = "PASSWORD";
 
 #endif
